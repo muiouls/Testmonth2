e2221c48f03107d26087a18baa0d8b038fc3b181
muiouls <127103649+muiouls@users.noreply.github.com>
2023-03-30 18:04:44 +0600

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,3 +1,11 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+class Hi(Hello):
+    def __init__(self,str):
+        super().__init__(str)
+
+new_Hello = ('Hello, world')
+
+
+print(new_Hello)
